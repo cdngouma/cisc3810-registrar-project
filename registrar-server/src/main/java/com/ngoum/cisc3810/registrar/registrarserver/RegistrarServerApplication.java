43e0807eb57e5915c493601cc32feb06f2c5b121
cdngouma <ngouma.chrys@gmail.com>
2019-07-21 01:45:36 +0100

    converted project to web service

---
+++ registrar-server/src/main/java/com/ngoum/cisc3810/registrar/registrarserver/RegistrarServerApplication.java
@@ -0,0 +1,13 @@
+package com.ngoum.cisc3810.registrar.registrarserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrarServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrarServerApplication.class, args);
+	}
+
+}
---
+++ registrar-server/src/test/java/com/ngoum/cisc3810/registrar/registrarserver/RegistrarServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ngoum.cisc3810.registrar.registrarserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RegistrarServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
